49dd25a3d73b7a72fc2dc251bc3b37a0e2ec9f1f
Harri Virtanen <harri@niodi.fi>
2024-08-17 22:09:44 +0300

    New linting + formatting (#1)
    * Test
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  plugins: ['prettier-plugin-css-order'],
+}
